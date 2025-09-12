67b627fb95c26d3a44f9b9f2f3d94ed77de7cd7a
NguyenHuuTuan <nguyenhuutuan1704@gmail.com>
2025-09-12 20:08:44 +0700

    Remove training script `train.sh` as it is no longer needed for the current workflow.

--- train.sh
+++
@@ -1,6 +0,0 @@
-python -m train \
-    --experiment_name WLASL100 \
-    --training_set_path datasets/WLASL100_train_25fps.csv \
-    --testing_set_path datasets/WLASL100_val_25fps.csv \
-    --validation_set split-from-train \
-    --num_classes 100
