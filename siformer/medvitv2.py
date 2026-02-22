337497c1615364f1f90ebc2984cccb8e476d8c24
Nguyễn Hữu Tuấn <nguyenhuutuan1704@gmail.com>
2026-02-22 15:34:48 +0700

    Chuyển hết sang nn.MultiheadAttention

--- siformer/medvitv2.py
+++ siformer/medvitv2.py
@@ -26,7 +26,7 @@ class LocalSelfAttention(nn.Module):
             d_model, nhead, dropout=dropout, batch_first=True
         )
 
-    def forward(self, x):
+    def forward(self, x, mask_padding=None):
         # x: [B, L, D]
         B, L, D = x.shape
         w = self.window_size
@@ -64,7 +64,7 @@ class LocalityFeedForward(nn.Module):
 
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x):
+    def forward(self, x, mask_padding=None):
         # x: [B, L, D]
         residual = x
         x = x.transpose(1, 2)          # [B, D, L]
@@ -117,7 +117,7 @@ class LocalLayer(nn.Module):
         self.rh_lffn = LocalityFeedForward(in_dim=d_model_list[1], expand_ratio=4., d_ff=d_ff, act=act, dropout=dropout)
         self.body_lffn = LocalityFeedForward(in_dim=d_model_list[2], expand_ratio=4., d_ff=d_ff, act=act, dropout=dropout)
 
-    def forward(self, l_hand, r_hand, body): # post-norm
+    def forward(self, l_hand, r_hand, body, mask_padding=None): # post-norm
         # src: [B, L, D]
 
         # Left hand
@@ -140,18 +140,26 @@ class GlobalLayer(nn.Module):
         super().__init__()
 
         # Giai đoạn 1: Self-Attention Layers
-        self.self_attn_lh = self_attn_list[0]
-        self.self_attn_rh = self_attn_list[1]
-        self.self_attn_body = self_attn_list[2]
+        # self.self_attn_lh = self_attn_list[0]
+        # self.self_attn_rh = self_attn_list[1]
+        # self.self_attn_body = self_attn_list[2]
+
+        self.self_attn_lh = nn.MultiheadAttention(d_model_list[0], nhead_list[0], kdim=d_model_list[0],
+                                                   vdim=d_model_list[0], batch_first=True)
+        self.self_attn_rh = nn.MultiheadAttention(d_model_list[1], nhead_list[1], kdim=d_model_list[1],
+                                                   vdim=d_model_list[1], batch_first=True)
+        self.self_attn_body = nn.MultiheadAttention(d_model_list[2], nhead_list[2], kdim=d_model_list[2],
+                                                   vdim=d_model_list[2], batch_first=True)
+
         self.norm1_lh = LayerNorm(d_model_list[0])
         self.norm1_rh = LayerNorm(d_model_list[1])
         self.norm1_body = LayerNorm(d_model_list[2])
 
         # rh truyền cho lh
         self.lh_from_rh_attn = nn.MultiheadAttention(d_model_list[0], nhead_list[0], kdim=d_model_list[1],
-                                                   vdim=d_model_list[1], dropout=dropout, batch_first=True)
+                                                   vdim=d_model_list[1], batch_first=True)
         self.rh_from_lh_attn = nn.MultiheadAttention(d_model_list[1], nhead_list[1], kdim=d_model_list[0],
-                                                   vdim=d_model_list[0], dropout=dropout, batch_first=True)
+                                                   vdim=d_model_list[0], batch_first=True)
 
         # Fusion layer chỉ nhận đầu ra từ một chú ý chéo
         self.lh_fusion_layer = nn.Linear(d_model_list[0], d_model_list[0])
@@ -175,7 +183,7 @@ class GlobalLayer(nn.Module):
         self.norm3_body = LayerNorm(d_model_list[2])
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, l_hand_x, r_hand_x, body_x): # post-norm
+    def forward(self, l_hand_x, r_hand_x, body_x, mask_padding=None): # post-norm
         # l_hand_x, r_hand_x, body_x: [B, L, D]
 
         # --- 1. Self-Attention ---
@@ -190,7 +198,7 @@ class GlobalLayer(nn.Module):
 
         # --- 2. Cross-Attention ---
         lh_from_rh, _ = self.lh_from_rh_attn(l_hand_x, r_hand_x, r_hand_x)
-        rh_from_lh, _ = self.rh_from_lh_attn(query=r_hand_x,key= l_hand_x, value=l_hand_x)
+        rh_from_lh, _ = self.rh_from_lh_attn(r_hand_x, l_hand_x, l_hand_x)
         l_hand_x = self.norm2_lh(l_hand_x + self.dropout(lh_from_rh))
         r_hand_x = self.norm2_rh(r_hand_x + self.dropout(rh_from_lh))
 
@@ -222,7 +230,7 @@ class LGBlock(nn.Module):
             self_attn_list=self_attn_list
         )
 
-    def forward(self, lh, rh, body):
+    def forward(self, lh, rh, body, mask_padding=None):
         # lh, rh, body: (B, L, D)
         lh, rh, body = self.local_layer(lh, rh, body)
         lh, rh, body = self.global_layer(lh, rh, body)
@@ -259,7 +267,7 @@ class CombinedLayer(nn.Module):
         ])
         logger(f'LGBlock = {num_layers}')
 
-    def forward(self, l_hand, r_hand, body):
+    def forward(self, l_hand, r_hand, body, mask_padding=None):
         # l_hand, r_hand, body: [B, L, D]
         for layer in self.layers:
             l_hand, r_hand, body = layer(l_hand, r_hand, body)
@@ -307,7 +315,7 @@ class SLMedViTV2(nn.Module):
         decoder_out = self.decoder(target, full_memory, training)
         return decoder_out
 
-    def forward(self, l_hand, r_hand, body):
+    def forward(self, l_hand, r_hand, body, mask_padding=None):
         batch_size = l_hand.size(0)
         training = self.training
 
