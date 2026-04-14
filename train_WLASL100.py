872aefed3c52788eaee444e604c5ff5379eebaae
Nguyễn Hữu Tuấn <nguyenhuutuan1704@gmail.com>
2026-04-14 16:28:47 +0700

    [WLASL100] [section 4.3] [1En 2De]

--- train_WLASL100.py
+++ train_WLASL100.py
@@ -553,7 +553,7 @@ if __name__ == "__main__":
         IA_decoder=True,
         num_worker=2,
         num_enc_layers=1,
-        num_dec_layers=1,
+        num_dec_layers=2,
         patience=0,
     )
 
